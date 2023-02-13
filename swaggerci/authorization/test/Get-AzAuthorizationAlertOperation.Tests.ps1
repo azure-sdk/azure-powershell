cb6e4cb04f34b074035b1a6a63d6acb410e1d1a1
SDKAuto <sdkautomation@microsoft.com>
2023-02-13 18:06:44 +0000

    CodeGen from PR 2790 in test-repo-billy/azure-rest-api-specs Merge 4e86cb44966ae1b15962de76d337af81ffc522fe into 50e7b9b2c1527ee2342032595541b2ffd12ae10d

---
+++ swaggerci/authorization/test/Get-AzAuthorizationAlertOperation.Tests.ps1
@@ -0,0 +1,29 @@
+if(($null -eq $TestName) -or ($TestName -contains 'Get-AzAuthorizationAlertOperation'))
+{
+  $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
+  if (-Not (Test-Path -Path $loadEnvPath)) {
+      $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
+  }
+  . ($loadEnvPath)
+  $TestRecordingFile = Join-Path $PSScriptRoot 'Get-AzAuthorizationAlertOperation.Recording.json'
+  $currentPath = $PSScriptRoot
+  while(-not $mockingPath) {
+      $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+      $currentPath = Split-Path -Path $currentPath -Parent
+  }
+  . ($mockingPath | Select-Object -First 1).FullName
+}
+
+Describe 'Get-AzAuthorizationAlertOperation' {
+    It 'List' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+
+    It 'Get' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+
+    It 'GetViaIdentity' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+}
