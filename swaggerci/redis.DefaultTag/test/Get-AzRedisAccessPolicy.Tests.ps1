8b690bfddbfbedfa1b3699b024570d2d0e5a823c
SDKAuto <sdkautomation@microsoft.com>
2023-04-21 06:43:25 +0000

    CodeGen from PR 23406 in Azure/azure-rest-api-specs Merge 036139db81d355316cd2346f3e0a5efd22f52c2e into ae5f241249f12e87e94e184ae5430518ac061a51

---
+++ swaggerci/redis.DefaultTag/test/Get-AzRedisAccessPolicy.Tests.ps1
@@ -0,0 +1,29 @@
+if(($null -eq $TestName) -or ($TestName -contains 'Get-AzRedisAccessPolicy'))
+{
+  $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
+  if (-Not (Test-Path -Path $loadEnvPath)) {
+      $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
+  }
+  . ($loadEnvPath)
+  $TestRecordingFile = Join-Path $PSScriptRoot 'Get-AzRedisAccessPolicy.Recording.json'
+  $currentPath = $PSScriptRoot
+  while(-not $mockingPath) {
+      $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+      $currentPath = Split-Path -Path $currentPath -Parent
+  }
+  . ($mockingPath | Select-Object -First 1).FullName
+}
+
+Describe 'Get-AzRedisAccessPolicy' {
+    It 'List' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+
+    It 'Get' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+
+    It 'GetViaIdentity' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+}
