868347fddfd12899227e1dc9c418bb2c5f8ff2c9
SDKAuto <sdkautomation@microsoft.com>
2024-02-28 23:28:48 +0000

    CodeGen from PR 27861 in Azure/azure-rest-api-specs Merge d6fcc46341f274b8af42a4cdcfa14e1f8d472619 into f428eaf0bbda62a0a5449ff16f7bb88a16411e33

---
+++ swaggerci/search.DefaultTag/test/Get-AzSearchNetworkSecurityPerimeterConfiguration.Tests.ps1
@@ -0,0 +1,29 @@
+if(($null -eq $TestName) -or ($TestName -contains 'Get-AzSearchNetworkSecurityPerimeterConfiguration'))
+{
+  $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
+  if (-Not (Test-Path -Path $loadEnvPath)) {
+      $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
+  }
+  . ($loadEnvPath)
+  $TestRecordingFile = Join-Path $PSScriptRoot 'Get-AzSearchNetworkSecurityPerimeterConfiguration.Recording.json'
+  $currentPath = $PSScriptRoot
+  while(-not $mockingPath) {
+      $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+      $currentPath = Split-Path -Path $currentPath -Parent
+  }
+  . ($mockingPath | Select-Object -First 1).FullName
+}
+
+Describe 'Get-AzSearchNetworkSecurityPerimeterConfiguration' {
+    It 'List' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+
+    It 'Get' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+
+    It 'GetViaIdentity' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+}
