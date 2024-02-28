57a54bf8f0871bb53de9dd6e41531ebe222b58c5
SDKAuto <sdkautomation@microsoft.com>
2024-02-28 23:33:10 +0000

    CodeGen from PR 27861 in Azure/azure-rest-api-specs Merge d6fcc46341f274b8af42a4cdcfa14e1f8d472619 into f428eaf0bbda62a0a5449ff16f7bb88a16411e33

---
+++ swaggerci/mysql.DefaultTag/test/Update-AzMySqlServer.Tests.ps1
@@ -0,0 +1,25 @@
+if(($null -eq $TestName) -or ($TestName -contains 'Update-AzMySqlServer'))
+{
+  $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
+  if (-Not (Test-Path -Path $loadEnvPath)) {
+      $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
+  }
+  . ($loadEnvPath)
+  $TestRecordingFile = Join-Path $PSScriptRoot 'Update-AzMySqlServer.Recording.json'
+  $currentPath = $PSScriptRoot
+  while(-not $mockingPath) {
+      $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+      $currentPath = Split-Path -Path $currentPath -Parent
+  }
+  . ($mockingPath | Select-Object -First 1).FullName
+}
+
+Describe 'Update-AzMySqlServer' {
+    It 'UpdateExpanded' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+
+    It 'UpdateViaIdentityExpanded' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+}
