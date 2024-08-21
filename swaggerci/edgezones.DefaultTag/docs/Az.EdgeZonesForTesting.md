ff83610effc57fb803021cf7b58094d7bc1e923a
SDKAuto <sdkautomation@microsoft.com>
2024-08-21 08:18:04 +0000

    CodeGen from PR 3461 in test-repo-billy/azure-rest-api-specs Merge b38a19c99c7c6841d4e936e3990928b5434a5a67 into c221294c903b4bcbe7b1ee4bce901322a7056943

---
+++ swaggerci/edgezones.DefaultTag/docs/Az.EdgeZonesForTesting.md
@@ -0,0 +1,22 @@
+---
+Module Name: Az.EdgeZonesForTesting
+Module Guid: a45864c8-0e70-49a7-8d28-1f063a55f150
+Download Help Link: https://learn.microsoft.com/powershell/module/az.edgezonesfortesting
+Help Version: 1.0.0.0
+Locale: en-US
+---
+
+# Az.EdgeZonesForTesting Module
+## Description
+Microsoft Azure PowerShell: EdgeZonesForTesting cmdlets
+
+## Az.EdgeZonesForTesting Cmdlets
+### [Get-AzEdgeZonesForTestingExtendedZone](Get-AzEdgeZonesForTestingExtendedZone.md)
+Gets an Azure Extended Zone for a subscription
+
+### [Register-AzEdgeZonesForTestingExtendedZone](Register-AzEdgeZonesForTestingExtendedZone.md)
+Registers a subscription for an Extended Zone
+
+### [Unregister-AzEdgeZonesForTestingExtendedZone](Unregister-AzEdgeZonesForTestingExtendedZone.md)
+Unregisters a subscription for an Extended Zone
+
---
+++ swaggerci/networkanalytics.DefaultTag/test/Remove-AzNetworkAnalyticsForTestingDataProduct.Tests.ps1
@@ -0,0 +1,25 @@
+if(($null -eq $TestName) -or ($TestName -contains 'Remove-AzNetworkAnalyticsForTestingDataProduct'))
+{
+  $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
+  if (-Not (Test-Path -Path $loadEnvPath)) {
+      $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
+  }
+  . ($loadEnvPath)
+  $TestRecordingFile = Join-Path $PSScriptRoot 'Remove-AzNetworkAnalyticsForTestingDataProduct.Recording.json'
+  $currentPath = $PSScriptRoot
+  while(-not $mockingPath) {
+      $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+      $currentPath = Split-Path -Path $currentPath -Parent
+  }
+  . ($mockingPath | Select-Object -First 1).FullName
+}
+
+Describe 'Remove-AzNetworkAnalyticsForTestingDataProduct' {
+    It 'Delete' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+
+    It 'DeleteViaIdentity' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+}
