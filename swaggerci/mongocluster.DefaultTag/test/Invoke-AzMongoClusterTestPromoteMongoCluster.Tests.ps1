783c60741a504f247f9b004d56f0dff264d2abdb
SDKAuto <sdkautomation@microsoft.com>
2024-10-23 03:41:37 +0000

    CodeGen from PR 3536 in test-repo-billy/azure-rest-api-specs Merge f568df0c21e46453a553de14124dd0918d28533c into 6c6e1713f03acaf37e5c89652b023dd2c9eae16e

---
+++ swaggerci/mongocluster.DefaultTag/test/Invoke-AzMongoClusterTestPromoteMongoCluster.Tests.ps1
@@ -0,0 +1,33 @@
+if(($null -eq $TestName) -or ($TestName -contains 'Invoke-AzMongoClusterTestPromoteMongoCluster'))
+{
+  $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
+  if (-Not (Test-Path -Path $loadEnvPath)) {
+      $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
+  }
+  . ($loadEnvPath)
+  $TestRecordingFile = Join-Path $PSScriptRoot 'Invoke-AzMongoClusterTestPromoteMongoCluster.Recording.json'
+  $currentPath = $PSScriptRoot
+  while(-not $mockingPath) {
+      $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+      $currentPath = Split-Path -Path $currentPath -Parent
+  }
+  . ($mockingPath | Select-Object -First 1).FullName
+}
+
+Describe 'Invoke-AzMongoClusterTestPromoteMongoCluster' {
+    It 'PromoteExpanded' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+
+    It 'Promote' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+
+    It 'PromoteViaIdentityExpanded' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+
+    It 'PromoteViaIdentity' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+}
