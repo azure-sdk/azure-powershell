6108a1c719bcf879219c71699b7e823484258837
SDKAuto <sdkautomation@microsoft.com>
2024-11-12 23:25:10 +0000

    CodeGen from PR 31508 in Azure/azure-rest-api-specs Merge 3037ca1debb0a052121c4c5ce087b66708fd2a4b into 34f16e1d44fcee88defac6d99899107ee67c945f

---
+++ swaggerci/containerservice.DefaultTag/test/New-AzContainerServiceLoadBalancer.Tests.ps1
@@ -0,0 +1,21 @@
+if(($null -eq $TestName) -or ($TestName -contains 'New-AzContainerServiceLoadBalancer'))
+{
+  $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
+  if (-Not (Test-Path -Path $loadEnvPath)) {
+      $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
+  }
+  . ($loadEnvPath)
+  $TestRecordingFile = Join-Path $PSScriptRoot 'New-AzContainerServiceLoadBalancer.Recording.json'
+  $currentPath = $PSScriptRoot
+  while(-not $mockingPath) {
+      $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+      $currentPath = Split-Path -Path $currentPath -Parent
+  }
+  . ($mockingPath | Select-Object -First 1).FullName
+}
+
+Describe 'New-AzContainerServiceLoadBalancer' {
+    It 'CreateExpanded' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+}
