9bf73ca32082af7eeb24802a27f691859967b854
SDKAuto <sdkautomation@microsoft.com>
2025-01-03 06:30:59 +0000

    CodeGen from PR 31983 in Azure/azure-rest-api-specs Merge baedf1fcc56187649923066f77923cb525252f0a into c37cd44037491c96dccd02454195bd6f3411f026

---
+++ swaggerci/cognitiveservices.DefaultTag/test/New-AzCognitiveServicesDefenderForAiSetting.Tests.ps1
@@ -0,0 +1,21 @@
+if(($null -eq $TestName) -or ($TestName -contains 'New-AzCognitiveServicesDefenderForAiSetting'))
+{
+  $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
+  if (-Not (Test-Path -Path $loadEnvPath)) {
+      $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
+  }
+  . ($loadEnvPath)
+  $TestRecordingFile = Join-Path $PSScriptRoot 'New-AzCognitiveServicesDefenderForAiSetting.Recording.json'
+  $currentPath = $PSScriptRoot
+  while(-not $mockingPath) {
+      $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+      $currentPath = Split-Path -Path $currentPath -Parent
+  }
+  . ($mockingPath | Select-Object -First 1).FullName
+}
+
+Describe 'New-AzCognitiveServicesDefenderForAiSetting' {
+    It 'CreateExpanded' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+}
