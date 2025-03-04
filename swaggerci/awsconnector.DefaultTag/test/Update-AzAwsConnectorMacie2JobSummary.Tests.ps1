a65ea8bcd02f421cd671307e7560650f7b9825bd
SDKAuto <sdkautomation@microsoft.com>
2025-03-04 20:51:20 +0000

    CodeGen from PR 32992 in Azure/azure-rest-api-specs Merge e33b8bfe81f6cf113e1d5fe38c28db45da219841 into 64b39af620f9326da0db6e012414838453be016c

---
+++ swaggerci/awsconnector.DefaultTag/test/Update-AzAwsConnectorMacie2JobSummary.Tests.ps1
@@ -0,0 +1,25 @@
+if(($null -eq $TestName) -or ($TestName -contains 'Update-AzAwsConnectorMacie2JobSummary'))
+{
+  $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
+  if (-Not (Test-Path -Path $loadEnvPath)) {
+      $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
+  }
+  . ($loadEnvPath)
+  $TestRecordingFile = Join-Path $PSScriptRoot 'Update-AzAwsConnectorMacie2JobSummary.Recording.json'
+  $currentPath = $PSScriptRoot
+  while(-not $mockingPath) {
+      $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+      $currentPath = Split-Path -Path $currentPath -Parent
+  }
+  . ($mockingPath | Select-Object -First 1).FullName
+}
+
+Describe 'Update-AzAwsConnectorMacie2JobSummary' {
+    It 'UpdateExpanded' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+
+    It 'UpdateViaIdentityExpanded' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+}
